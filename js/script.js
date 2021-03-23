4136dbf55acb4472aaecfaf5155ef70dc415125e
ZaBo <patrizia@zaio.it>
2021-03-23 23:58:50 +0100

    feat: insert the stars in the right box

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-// set the margin at rate elements //
+"use strict";
 
 const rates = document.getElementById("rates");
-const numberOfChildren = rates.children.length;
+const rateElement = document.querySelectorAll(".comment");
+
+// set the margin at rate elements //
+const numberOfChildren = rateElement.length;
 const offset = 3;
-console.log(rates.children[1]);
+
 for (let i = 0; i < numberOfChildren; i++) {
-  rates.children[i].style.margin =
-    "1em " + (numberOfChildren - i) * offset + "em 1em " + i * offset + "em";
+  rateElement[i].style.margin = `1em ${(numberOfChildren - i) * offset}em 1em ${
+    i * offset
+  }em`;
 }
