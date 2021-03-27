b5775b4c81219cb612dbae91c72c5a6021b6e505
ZaBo <patrizia@zaio.it>
2021-03-27 14:36:54 +0100

    feat: set the animation for all the elements with js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 "use strict";
 
 const rates = document.getElementById("rates");
-const rateElement = document.querySelectorAll(".comment");
+const rateElement = document.querySelectorAll(".rate");
+const card = document.querySelectorAll(".card");
 
-// set the margin at rate elements //
-const numberOfChildren = rateElement.length;
-const offset = 3;
+const animationDelay = 0.5;
+const ratesOffset = 3;
+const cardOffset = 1;
 
-for (let i = 0; i < numberOfChildren; i++) {
-  rateElement[i].style.margin = `1em ${(numberOfChildren - i) * offset}em 1em ${
-    i * offset
-  }em`;
+// set the margin and the animation delay at rate elements //
+const numberOfRates = rateElement.length;
+
+for (let i = 0; i < numberOfRates; i++) {
+  rateElement[i].style.margin = `1em ${
+    (numberOfRates - i) * ratesOffset
+  }em 1em ${i * ratesOffset}em`;
+
+  rateElement[i].style.animationDelay = `${animationDelay * i}s`;
+}
+
+// set the margin and the anmation delaty at the card
+const numberOfCards = card.length;
+
+for (let i = 0; i < numberOfCards; i++) {
+  card[i].style.margin = ` ${4 + i * cardOffset}em 0 ${
+    numberOfCards - i * cardOffset
+  }em 0`;
+
+  card[i].style.animationDelay = `${
+    animationDelay * i + animationDelay * numberOfRates
+  }s`;
 }
