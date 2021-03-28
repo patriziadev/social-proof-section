2efaceea31396129cf69456ed4927e5b5ed9316b
ZaBo <patrizia@zaio.it>
2021-03-28 23:32:03 +0200

    feat: set mobile view

--- js/script.js
+++ js/script.js
@@ -7,27 +7,32 @@ const card = document.querySelectorAll(".card");
 const animationDelay = 0.5;
 const ratesOffset = 3;
 const cardOffset = 1;
+const mobileMaxWidth = 992;
 
 // set the margin and the animation delay at rate elements //
 const numberOfRates = rateElement.length;
 
-for (let i = 0; i < numberOfRates; i++) {
-  rateElement[i].style.margin = `1em ${
-    (numberOfRates - i) * ratesOffset
-  }em 1em ${i * ratesOffset}em`;
+if (window.innerWidth > mobileMaxWidth) {
+  for (let i = 0; i < numberOfRates; i++) {
+    rateElement[i].style.margin = `1em ${
+      (numberOfRates - i) * ratesOffset
+    }em 1em ${i * ratesOffset}em`;
 
-  rateElement[i].style.animationDelay = `${animationDelay * i}s`;
+    rateElement[i].style.animationDelay = `${animationDelay * i}s`;
+  }
 }
 
 // set the margin and the anmation delaty at the card
 const numberOfCards = card.length;
 
-for (let i = 0; i < numberOfCards; i++) {
-  card[i].style.margin = ` ${4 + i * cardOffset}em 0 ${
-    numberOfCards - i * cardOffset
-  }em 0`;
+if (window.innerWidth > mobileMaxWidth) {
+  for (let i = 0; i < numberOfCards; i++) {
+    card[i].style.margin = ` ${i * cardOffset}em 0 ${
+      numberOfCards - i * cardOffset
+    }em 0`;
 
-  card[i].style.animationDelay = `${
-    animationDelay * i + animationDelay * numberOfRates
-  }s`;
+    card[i].style.animationDelay = `${
+      animationDelay * i + animationDelay * numberOfRates
+    }s`;
+  }
 }
